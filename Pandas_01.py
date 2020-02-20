9d6315f44f32a194fa3bf7ee1e2836c9ee1b84f4
NingYYao <49973719+NingYYao@users.noreply.github.com>
2020-02-20 15:32:00 +0900

    Create Pandas_01.py

---
+++ Pandas_01.py
@@ -0,0 +1,81 @@
+import pandas as pd
+
+df = pd.DataFrame({'A': [11, 21, 31],
+                   'B': [12, 22, 32],
+                   'C': [13, 23, 33]},
+                  index=['ONE', 'TWO', 'THREE'])
+
+print(df)
+#         A   B   C
+# ONE    11  12  13
+# TWO    21  22  23
+# THREE  31  32  33
+
+'''rename()'''
+df_new = df.rename(columns={'A': 'a'}, index={'ONE': 'one'})
+print(df_new)
+#         a   B   C
+# one    11  12  13
+# TWO    21  22  23
+# THREE  31  32  33
+
+print(df)
+#         A   B   C
+# ONE    11  12  13
+# TWO    21  22  23
+# THREE  31  32
+
+'''多个行名和列名的修改'''
+print(df.rename(columns={'A': 'a', 'C': 'c'}))
+#         a   B   c
+# ONE    11  12  13
+# TWO    21  22  23
+# THREE  31  32  33
+
+'''原DataFrame的修改（参数inplace）'''
+df_org = df.copy()
+df_org.rename(columns={'A': 'a'}, index={'ONE': 'one'}, inplace=True)
+print(df_org)
+#         a   B   C
+# one    11  12  13
+# TWO    21  22  23
+# THREE  31  32  33
+
+'''使用lambda表达式和函数进行批处理'''
+print(df.rename(columns=str.lower, index=str.title))
+#         a   b   c
+# One    11  12  13
+# Two    21  22  23
+# Three  31  32  33
+
+print(df.rename(columns=lambda s: s*3, index=lambda s: s + '!!'))
+#          AAA  BBB  CCC
+# ONE!!     11   12   13
+# TWO!!     21   22   23
+# THREE!!   31   32   33
+
+'''DataFrame.add_prefix(), DataFrame.add_suffix()'''
+print(df.add_prefix('X_'))
+#        X_A  X_B  X_C
+# ONE     11   12   13
+# TWO     21   22   23
+# THREE   31   32   33
+
+print(df.add_suffix('_X'))
+#        A_X  B_X  C_X
+# ONE     11   12   13
+# TWO     21   22   23
+# THREE   31   32   33
+
+'''index和columns元素的更新'''
+df.index = [1, 2, 3]
+df.columns = ['a', 'b', 'c']
+
+print(df)
+#     a   b   c
+# 1  11  12  13
+# 2  21  22  23
+# 3  31  32  33
+
+# df.index = [1, 2, 3, 4]
+# ValueError: Length mismatch: Expected axis has 3 elements, new values have 4 elements
