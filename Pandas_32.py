37ca2c6214f6059fa23b42664892f1c1db05c11d
NYao925 <lssac0925@hotmail.com>
2020-12-28 15:41:45 +0900

    Create Pandas_32.py

---
+++ Pandas_32.py
@@ -0,0 +1,13 @@
+#『Python Data Science Handbook』对一下内容进行了详细的总结
+#* Numpy
+#* Pandas
+#* Matplotlib
+#* Scikit-Learn
+
+#值得庆幸的是，在线全文可以在网上找到。图表也按原样显示。
+#* [Python Data Science Handbook](https://jakevdp.github.io/PythonDataScienceHandbook/)
+
+#还提供Jupyter Notebook.ipynb格式的示例。不仅包括代码，还包括文本以及所有图形和表格。以下存储库中的笔记本中有一个分节文件。
+#* [jakevdp/PythonDataScienceHandbook: Python Data Science Handbook: full text in Jupyter Notebooks](https://github.com/jakevdp/PythonDataScienceHandbook)
+
+#当您在GitHub上打开每个.ipynb文件时，该文件将被完整地呈现。
