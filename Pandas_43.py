b67cec61d3ad86c31f823bef250b6acfe5620698
NingYao <lssac0925@hotmail.com>
2021-05-16 16:05:27 +0900

    Create Pandas_43.py

---
+++ Pandas_43.py
@@ -0,0 +1,64 @@
+import pandas as pd
+
+print(pd.__version__)
+# 1.0.3
+
+pd.show_versions()
+#INSTALLED VERSIONS
+#------------------
+#commit           : None
+#python           : 3.7.4.final.0
+#python-bits      : 64
+#OS               : Darwin
+#OS-release       : 20.3.0
+#machine          : x86_64
+#processor        : i386
+#byteorder        : little
+#LC_ALL           : None
+#LANG             : None
+#LOCALE           : en_US.UTF-8
+
+#pandas           : 1.0.3
+#numpy            : 1.18.2
+#pytz             : 2019.3
+#dateutil         : 2.8.1
+#pip              : 19.0.3
+#setuptools       : 40.8.0
+#Cython           : None
+#pytest           : None
+#hypothesis       : None
+#sphinx           : None
+#blosc            : None
+#feather          : None
+#xlsxwriter       : None
+#lxml.etree       : None
+#html5lib         : None
+#pymysql          : None
+#psycopg2         : None
+#jinja2           : None
+#IPython          : None
+#pandas_datareader: None
+#bs4              : None
+#bottleneck       : None
+#fastparquet      : None
+#gcsfs            : None
+#lxml.etree       : None
+#matplotlib       : None
+#numexpr          : None
+#odfpy            : None
+#openpyxl         : None
+#pandas_gbq       : None
+#pyarrow          : None
+#pytables         : None
+#pytest           : None
+#pyxlsb           : None
+#s3fs             : None
+#scipy            : None
+#sqlalchemy       : None
+#tables           : None
+#tabulate         : None
+#xarray           : None
+#xlrd             : None
+#xlwt             : None
+#xlsxwriter       : None
+#numba            : None
